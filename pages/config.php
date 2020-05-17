33767e7b7de34c2ce6b20b74c3951932f6b945fa
DominicPuzzuoli <65130465+DominicPuzzuoli@users.noreply.github.com>
2020-05-17 00:09:47 -0400

    Add files via upload

---
+++ pages/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'puzzuoli');
+    define('DB_PASSWORD', 'Ahkeenah4c');
+    define('DB_DATABASE', 'puzzuoli');
+    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
